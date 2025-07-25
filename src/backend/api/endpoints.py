692fab4a56e369e8cd8867bd379233c9f1d8c418
myalla <mahanthyalla@iisc.ac.in>
2025-07-26 00:57:25 +0530

    ddpm_update 01

--- src/backend/api/endpoints.py
+++ src/backend/api/endpoints.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, UploadFile, File
 from PIL import Image, ImageFilter
 import io
-
 from fastapi.responses import StreamingResponse
+from models.ddpm import PretrainedDDPM, CustomDDPM
+from models.object_detect import PretrainedODM, CustomODM
 
 router = APIRouter()
 
@@ -11,8 +12,41 @@ async def health_check():
     return {"status": "healthy"}
 
 @router.post("/inpaint")
-async def inpaint_image(image: str):
-    return {"message": "Inpainting not implemented yet."}
+async def inpaint_image(
+        image: UploadFile = File(...), 
+        # mask: UploadFile = File(...), 
+        prompt: str = "", 
+        verbose: bool = True
+    ):
+    try:
+        image_contents = await image.read()
+        # mask_contents = await mask.read()
+        
+        mask_contents = image_contents  # For testing, using the same image as mask
+        
+        img = Image.open(io.BytesIO(image_contents)).convert("RGB")
+        mask_img = Image.open(io.BytesIO(mask_contents)).convert("L")
+        if verbose:
+            print(f"Mask image size: {mask_img.size}, Image size: {img.size}")
+            print(f"Prompt: {prompt}")
+            print(f"Type of mask image: {type(img)}")
+            print(f"Type of mask image: {type(mask_img)}")
+        
+        ddpm_model = PretrainedDDPM(pretrained_model_name=0)
+        inpainted_img = ddpm_model.inference({
+            'image': img,
+            'mask': mask_img,
+            'prompt': prompt
+        })
+        if verbose:
+            print(f"Inpainted image type: {type(inpainted_img)}")
+        buffer = io.BytesIO()
+        inpainted_img.save(buffer, format="PNG")
+        buffer.seek(0)
+        return StreamingResponse(buffer, media_type="image/png")
+    except Exception as e:
+        return {"error": str(e)}
+        # return {"message": "Inpainting not implemented yet."}
 
 
 @router.post("/invertimage")
